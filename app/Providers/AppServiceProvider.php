2979d22892eb7079d369e3f5d7897159d6186af3
khushali985 <khushali.220180107039@gmail.com>
2026-03-25 12:32:26 +0530

    fix https mixed content issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 {
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+    
     Paginator::useBootstrapFive();
 }
 }

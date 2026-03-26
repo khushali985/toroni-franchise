0da1c87d40bec6bd9c1bb03a83a692734e7fc901
khushali985 <khushali.220180107039@gmail.com>
2026-03-26 11:24:17 +0530

    adujsted the media query for 1366 and no of storys

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
         $stories = Story::latest()->get();
 
         // Fetch latest 5 reviews (read-only)
-        $reviews = Review::latest()->take(4)->get();
+        $reviews = Review::latest()->take(5)->get();
 
         return view('pages.home', compact('stories', 'reviews'));
     }
